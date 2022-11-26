25cb180edf8018c3d08e25941a98126e84578312
Ilya Andreev <Franklylakers@yandex.ru>
2022-11-26 20:23:15 +0700

    Update Example4

--- Example4/Program.cs
+++ Example4/Program.cs
@@ -1,13 +1,16 @@
-﻿int a = 44;
-int b = 5;
-int c = 78;
+﻿Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Enter third number: ");
+int c = int.Parse(Console.ReadLine());
 int max = a;
 
-if (a<b)
+if (a < b)
 {
     max = b;
 }
-if (a<c)
+if (a < c)
 {
     max = c;    
 }
