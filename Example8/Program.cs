2d78eab62bab34713d174e518d554db6f3e22a9b
Ilya Andreev <Franklylakers@yandex.ru>
2022-11-26 20:41:45 +0700

    add Example8

---
+++ Example8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter number: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N > 0)
+{
+    for (int i = 2; i <= N; i += 2)
+    {
+        Console.WriteLine(i);
+    }
+    
+}
+else
+{
+    Console.WriteLine("Invaild value");
+}
