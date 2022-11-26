e1aebea39cc42f45c9104247e5b4eb11fe3e9026
Ilya Andreev <Franklylakers@yandex.ru>
2022-11-26 20:52:36 +0700

    Update Example2

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -3,15 +3,27 @@ int firstNum = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int secondNum = int.Parse(Console.ReadLine());
 int max = firstNum;
+int min = secondNum;
 
 if( firstNum > secondNum )
 {
     max = firstNum;
+    min = secondNum;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.WriteLine(min);
 }
-else
+if( secondNum > firstNum)
 {
     max = secondNum;
+    min = firstNum;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.WriteLine(min);
 }
-
-Console.Write("max = ");
-Console.WriteLine(max);
+if(firstNum == secondNum)
+{
+    Console.WriteLine("The numbers are equal");
+}
