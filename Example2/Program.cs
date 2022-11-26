3ffdbe4ca1ab242ad3ec7867be40486de37dc84c
Ilya Andreev <Franklylakers@yandex.ru>
2022-11-26 20:14:20 +0700

    add Example 2

---
+++ Example2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNum = int.Parse(Console.ReadLine());
+int max = firstNum;
+
+if( firstNum > secondNum )
+{
+    max = firstNum;
+}
+else
+{
+    max = secondNum;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
