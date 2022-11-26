213a5c0265f3435719ebb8e2a3916ec7d0a6e19b
Ilya Andreev <Franklylakers@yandex.ru>
2022-11-26 20:15:19 +0700

    add Example 6

---
+++ Example6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your number: ");
+int num = int.Parse(Console.ReadLine());
+
+if ((num % 2) == 0)
+{
+    Console.WriteLine("Your number is even");
+}
+else
+{
+    Console.WriteLine("Your number is odd");
+}
